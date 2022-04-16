9c77e2a585b86a5fe27dc8f22e644b347300a8f8
Peppermintsaregood <103156717+Peppermintsaregood@users.noreply.github.com>
2022-04-16 09:54:09 -0400

    Update DOCUMENTATION.lua

--- DOCUMENTATION.lua
+++ DOCUMENTATION.lua
@@ -2,6 +2,7 @@
 --addHistory(text) -- adds an item to the scrollframe menu using the text given
 --addCmd(text, function) -- adds a cmd to the cmd table, and runs the function given when cmd called
 --start() -- starts all functions(run at end)
+--killUI() -- removes ui
 -- example
 local module = loadstring(game:HttpGet("https://raw.githubusercontent.com/Peppermintsaregood/CMD-MODULE/main/source.lua"))() -- call the module
 module.createGui()
@@ -12,3 +13,5 @@ module.addCmd(";yes", function()
 end)
 wait(0.1)
 module.start()
+wait(5)
+module.killUI()
